ded07445f54a0cc06871c85a1049567a8aef1103
Milene <milenefaria33@gmail.com>
2024-03-11 22:19:22 -0300

    creating homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,17 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../layout/LinkButton'
+
 function Home() {
-    return <h1>Home</h1>
+    return (
+        <section className={styles.home_container}>
+            <h1>Welcome to <spam>Costs</spam></h1>
+            <p>Get started to managing your project right now!</p>
+
+            <LinkButton to="/new-project" text="Create project" />
+            <img src={savings} alt="Costs" />
+        </section>
+    )
 }
 
 export default Home
